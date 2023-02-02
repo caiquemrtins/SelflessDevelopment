f5bb087f8b5ec4ecd33e75270cb3f3ebadb2f512
Caique Carvalho <caiquemrtins@gmail.com>
2023-02-02 15:17:57 -0300

    First Commit

---
+++ DriverSQL/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+
+    static String URL = "jdbc:mysql://localhost:3306/testsql";
+    static String usuario = "root";
+    static String password = "celular1";
+
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, usuario, password);
+
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO `testsql`.`peoples`\n" +
+                    "(`firstname`)\n" +
+                    "VALUES\n" +
+                    "('jovem');");
+            stmt.execute();
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from peoples");
+
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("id") +" | " + resultSet.getString("firstname"));
+
+            }
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
